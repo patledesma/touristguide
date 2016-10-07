c3bf49e56b9021fb9c032391d6215386d47584d4
omiplekevin <omiple.kevin@gmail.com>
2016-10-07 20:26:14 +0800

    Serialization

--- app/src/main/java/com/sulkud/touristguide/models/RouteModel.java
+++ app/src/main/java/com/sulkud/touristguide/models/RouteModel.java
@@ -1,24 +1,39 @@
 package com.sulkud.touristguide.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class RouteModel {
 
+    @SerializedName("destination_addresses")
     public String destination_addresses;
 
+    @SerializedName("origin_addresses")
     public String origin_addresses;
 
+    @SerializedName("rows")
     public Elements[] rows;
 
+    @SerializedName("status")
     public String status;
 
     public class Elements {
 
+        @SerializedName("distance")
         public DistanceDuration distance;
 
+        @SerializedName("duration")
         public DistanceDuration duration;
+
+        @SerializedName("status")
+        public String status;
     }
 
     public class DistanceDuration {
+
+        @SerializedName("text")
         public String text;
+
+        @SerializedName("value")
         public int value;
     }
 
